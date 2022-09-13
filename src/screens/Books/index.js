a4184c5494157f06e11660f9acf2e5a56a9fa0ac
thiago morgado <thiago_morgado@hotmail.com>
2022-09-13 13:20:13 -0300

    implementando loading do books

--- src/screens/Books/index.js
+++ src/screens/Books/index.js
@@ -1,32 +1,40 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import api from '../../services/api';
 
 import { NavBar } from '../../components/Navbar';
 import { Card } from '../../components/Card';
 
-export function BooksPage(){
+export function BooksPage() {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
     let isSubscribed = true
     async function getBooksSql() {
       const response = await api.get('/livros');
       if (isSubscribed) {
         setBooks(response.data);
-      }        
+        setLoading(false);
+      }
     };
     getBooksSql();
     return () => isSubscribed = false
     // eslint-disable-next-line
   }, []);
- 
-  return(
+
+  return (
     <>
       <NavBar />
       <h1>TODOS OS LIVROS!</h1>
-      {books?.slice(0, 3).map((book, index) => {
-        return <Card key={index} product={book} /> 
-      })} 
+
+
+      {
+
+        loading ? <h2>Aguardando...</h2> :
+          books?.slice(0, 3).map((book, index) => {
+            return <Card key={index} product={book} />
+          })
+      }
     </>
   );
 }
